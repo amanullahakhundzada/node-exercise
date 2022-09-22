2b46767de58158ce668b0399b7e1f4ddfb5f0adb
amanullahakhundzada <amanullahkhan9631@gmail.com>
2022-09-22 10:48:16 -0700

    Express

---
+++ routes/users.routes.js
@@ -0,0 +1,51 @@
+import express  from "express";
+import db from "../mockdb";
+
+const router = express.Router();
+
+router.get("/:id?", async (req, res, next) => {
+  try {
+    let { id } = req.params.id;
+    let data;
+    if (id) {
+      data = await db.getOne(id);
+    } else {
+      data = await db.getAll();
+    }
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    let newUser= req.body;
+    let data = await db.add();
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    let {id} = req.params;
+    let updatedUser = req.body;
+    let data = await db.update(id,updatedUser)
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async(req, res, next) => {
+  try {
+    let {id}=req.params;
+    let data = await data.remove(id)
+    res.json(data)
+  } catch (error) {
+    next(error);
+  }
+});
+export default router;
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import express from "express";
+import morgan from "morgan";
 import config from "./config";
+// import router from "./route";
+import apiRouter from "./routes";
+import path from "path";
 // TODO: import router from routes/
 
 const app = express();
 
 app.use(express.json());
-
+app.use(morgan("common"));
+app.use(express.static(path.join(__dirname,"./public")))
 // TODO: use the imported router to handle all requests
-
+app.use("/api",apiRouter);
 app.use((err, req, res, next) => {
   console.error(err);
   res.json({ name: err.name, msg: err.message });
